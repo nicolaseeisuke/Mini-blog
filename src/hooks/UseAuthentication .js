fcbc476a8ef8b15f2951b5821682803280d143b1
nicolaseeisuke <nicolaseeisuke@gmail.com>
2023-07-05 19:03:51 -0300

    authentication login

--- src/hooks/UseAuthentication .js
+++ src/hooks/UseAuthentication .js
@@ -75,12 +75,39 @@ export const UseAuthentication = () => {
 
     checkIfCancelled()  
     signOut(auth)
+  }
 
+  // login
+  const login = async(data) => {
+      checkIfCancelled()
+
+      setLoading(true)
+      setError(false)
+
+      try{
+
+        await signInWithEmailAndPassword(auth, data.email, data.password)
+        setLoading(false)
+
+      }catch(error){
+        let systemErrorMessage;
+        
+        if(error.message.includes("user-not-found")){
+          systemErrorMessage = 'Usuário não encontrado'
+        }else if(error.message.includes("wrong-password")){
+          systemErrorMessage = "Senha incorreta"
+        }else{
+          systemErrorMessage = "Ocorreu um erro tente novamente mais tarde "
+        }
+
+        setError(systemErrorMessage)
+        setLoading(false)
+      }
   }
 
   useEffect(() => {
     return () => setCancelled(true)
   }, [])
 
-  return {auth, createUser, error, loading, logout}
+  return {auth, createUser, error, loading, logout, login}
 }
